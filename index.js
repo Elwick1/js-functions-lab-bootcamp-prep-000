c1afeac368ae27c3037e81de0f073aee63ffe647
Michael Williamson <michaelrwilliamson92@gmail.com>
2019-07-16 11:16:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// write your code below!
+function happyHolidays(string) {
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo(string) {
+  return "Happy holidays, you!"
+}
+
+function happyHolidayTo(holiday, name) {
+  console.log(holiday, name)
+  return "Happy `${Independance Day}, ${You}!`"
+}
